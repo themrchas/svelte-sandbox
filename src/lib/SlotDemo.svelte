801644d8b7e7c6c83e7457157a10669cf6611fa7
Charles Geib <Charles.Geib@devops.ncia.nato.int>
2025-03-21 14:30:08 +0100

    more changes

--- src/lib/SlotDemo.svelte
+++ src/lib/SlotDemo.svelte
@@ -2,6 +2,14 @@
 
     console.log("Use $$slots tells you what slot names have been passed.  Current value is",$$slots)
 
+    const onmouseenter = (event:Event) => {
+        console.log('mouse just entered the slot "content" area')
+    }
+
+    const onmouseleave = (event:Event) => {
+        console.log('mouse just left the slot "content" area')
+    }
+
 </script>
 
 <div>
@@ -12,11 +20,13 @@
         This is a default slot header in SlotDemo
         <br/>
     </slot>
+    <div {onmouseenter} {onmouseleave}>
     <slot name="content">
         This is default slot for content in SlotDemo
         <br/>
-    </slot>
-    <slot name="footer">
+        </slot>
+    </div>
+       <slot name="footer">
         This is a default slot footer in SlotDemo
         <br/>
     </slot>
